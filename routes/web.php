9eed61c49c342bc69c9cc6f8aa18179976c29863
michihiro <exapua317@yahoo.co.jp>
2019-03-12 23:00:29 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+  });
+
+Route::get('hello','HelloController@index');
+
+Route::get('keijibanlara','KeijibanController@index');
+Route::get('keijibanlara/{name?}/{msg?}','KeijibanController@index');
+Route::post('keijibanlara','KeijibanController@post');
+
